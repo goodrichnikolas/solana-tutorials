db692c1a15f55cb365067f919007f0de0328c9b5
Nikolas Goodrich <goodrichnik@gmail.com>
2024-11-27 04:48:00 +0000

    Before

--- dist/connect-node.js
+++ dist/connect-node.js
@@ -2,12 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const web3_js_1 = require("@solana/web3.js");
 async function main() {
-    const connection = new web3_js_1.Connection((0, web3_js_1.clusterApiUrl)("devnet"));
-    const address = new web3_js_1.PublicKey("CenYq6bDRB7p73EjsPEpiYN7uveyPUTdXkDkgUduboaN");
+    const connection = new web3_js_1.Connection((0, web3_js_1.clusterApiUrl)("mainnet-beta"));
+    const address = new web3_js_1.PublicKey("F5QHcqE7B8Br6GCGjWKuCMy1zmJnhhgNztAkeZWvuziS");
     const balance = await connection.getBalance(address);
-    const balance_in_lamports = balance / 1000000000;
+    const balance_in_sol = balance / 1000000000;
     console.log(`The balance of the account at ${address} is ${balance} lamports`);
-    console.log(`This is the equivalent of ${balance_in_lamports} SOL`);
+    console.log(`This is the equivalent of ${balance_in_sol} SOL`);
     console.log(`✅ Finished!`);
 }
 main().catch(err => {
--- src/connect-node.ts
+++ src/connect-node.ts
@@ -1,14 +1,29 @@
 import { Connection, PublicKey, clusterApiUrl } from "@solana/web3.js";
 
 
+const public_key = "F5QHcqE7B8Br6GCGjWKuCMy1zmJnhhgNztAkeZWvuziS";
+
+//Function that queries all transactions of a given account
+async function getTransactionHistory(address) {
+    //Connect to server and provide it the key
+    const connection = new Connection(clusterApiUrl("mainnet-beta"));
+    const pubKey = new PublicKey(address);
+
+    try {
+        //Get all transactions of the account
+        const signatures = await connection.getSignaturesForAddress(pubKey);
+        console.log("Signatures: ", signatures);
+    }
+}
 async function main() {
-    const connection = new Connection(clusterApiUrl("devnet"));
-    const address = new PublicKey("CenYq6bDRB7p73EjsPEpiYN7uveyPUTdXkDkgUduboaN");
+    
+    const connection = new Connection(clusterApiUrl("mainnet-beta"));
+    const address = new PublicKey(public_key);
     const balance = await connection.getBalance(address);
-    const balance_in_lamports = balance / 1000000000;
+    const balance_in_sol = balance / 1000000000;
 
     console.log(`The balance of the account at ${address} is ${balance} lamports`);
-    console.log(`This is the equivalent of ${balance_in_lamports} SOL`);
+    console.log(`This is the equivalent of ${balance_in_sol} SOL`);
     console.log(`✅ Finished!`);
 }
 
