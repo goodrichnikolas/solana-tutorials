5741f2d9b96944a140d6f94fafeee3c23543eccd
Nikolas Goodrich <goodrichnik@gmail.com>
2024-11-27 02:10:20 +0000

    keygen made

--- dist/keygen.js
+++ dist/keygen.js
@@ -1,7 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const web3_js_1 = require("@solana/web3.js");
-const keypair = web3_js_1.Keypair.generate();
+const helpers_1 = require("@solana-developers/helpers");
+require("dotenv/config");
+//const keypair = Keypair.generate();
+//console.log(`The public key is: `, keypair.publicKey.toBase58());
+//console.log(`The secret key is: `, keypair.secretKey);
+const keypair = (0, helpers_1.getKeypairFromEnvironment)("SECRET_KEY");
+console.log(`✅ Finished! We've loaded our secret key securely, using an env file!`);
 console.log(`The public key is: `, keypair.publicKey.toBase58());
 console.log(`The secret key is: `, keypair.secretKey);
 //# sourceMappingURL=keygen.js.map
--- src/keygen.ts
+++ src/keygen.ts
@@ -1,6 +1,16 @@
 import { Keypair } from "@solana/web3.js";
+import { getKeypairFromEnvironment } from "@solana-developers/helpers";
+import "dotenv/config"; 
+//const keypair = Keypair.generate();
  
-const keypair = Keypair.generate();
+//console.log(`The public key is: `, keypair.publicKey.toBase58());
+//console.log(`The secret key is: `, keypair.secretKey);
  
+const keypair = getKeypairFromEnvironment("SECRET_KEY");
+ 
+console.log(
+  `✅ Finished! We've loaded our secret key securely, using an env file!`,
+);
+
 console.log(`The public key is: `, keypair.publicKey.toBase58());
 console.log(`The secret key is: `, keypair.secretKey);
